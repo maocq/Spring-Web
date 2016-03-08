59c900bbdcaa28de9a8a03e167b2c3d84bea7855
maocq <jmcarmona@smdigital.com.co>
2016-03-07 23:15:56 -0500

    gson

--- src/co/com/maocq/controller/IndexController.java
+++ src/co/com/maocq/controller/IndexController.java
@@ -1,14 +1,27 @@
 package co.com.maocq.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+
+import co.com.maocq.beans.Usuario;
 
 @Controller
 public class IndexController {
 
-  @RequestMapping(value = "/")
-  public @ResponseBody String showAbout() {    
-    return "index";
-  }
+	final static Gson gson = new Gson();
+
+	@RequestMapping(value = "/", method = RequestMethod.POST, produces = (MediaType.APPLICATION_JSON_VALUE + ";charset=utf-8"))
+	public @ResponseBody String showAbout() {
+		
+		Usuario usuario = new Usuario();
+		usuario.setName("Mauricio");
+		usuario.setEmail("carmonaesc@gmail.com");
+
+		return gson.toJson(usuario);
+	}
 }
